451347fae1f94c1325bcf71e01a7028986e7beee
mayara_Abreu <mayara.arj@gmail.com>
2022-04-14 09:46:32 -0300

    Criação exercício breakContinue

---
+++ breakContinue.js
@@ -0,0 +1,14 @@
+//Break e Continue - Números Pares
+//Precisamos imprimir somente os números pares de 0 a 20. Mas temos alguns requisitos, você precisa utilizar, o loop for, o comando continue e o número deve ser o contador do loop! Vamos lá?
+
+//Dica: no seu for, utilize como contador uma variável chamada i.
+
+for(var i=0; i<=20; ++i){
+
+    if(i%2===0)
+  
+        console.log(i)
+  
+    continue
+  
+  }
